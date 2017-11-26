defc826d127dd4515c2e1cdcd43c63db9be29613
Ondrej Skala <ondra1237@gmail.com>
2017-11-26 23:17:00 +0100

    Represents identifier

---
+++ src/Identifier.java
@@ -0,0 +1,23 @@
+
+/**
+ * Represents identifier
+ *
+ */
+public abstract class Identifier {
+	
+	private String name;
+	
+	public Identifier(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
