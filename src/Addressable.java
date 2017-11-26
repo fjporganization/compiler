d8db045f84a56d0a585491c1f9bd03a53369f9db
Ondrej Skala <ondra1237@gmail.com>
2017-11-26 23:09:56 +0100

    Represents addressable item on stack

---
+++ src/Addressable.java
@@ -0,0 +1,43 @@
+/**
+ * Represents addressable item
+ *
+ */
+public abstract class Addressable extends Identifier{
+
+	private int address;
+	
+	private int nestingLevel;
+	
+	private int length;
+	
+	public Addressable(int address, int nestingLevel, String name, int lenght) {
+		super(name);
+		this.address = address;
+		this.nestingLevel = nestingLevel;
+		this.length = length;
+	}
+
+	public int getAddress() {
+		return address;
+	}
+
+	public void setAddress(int address) {
+		this.address = address;
+	}
+
+	public int getNestingLevel() {
+		return nestingLevel;
+	}
+
+	public void setNestingLevel(int nestingLevel) {
+		this.nestingLevel = nestingLevel;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+}
