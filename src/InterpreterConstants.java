3a5d670180914198feaa2f79f000ee5bec07317e
Ondrej Skala <ondra1237@gmail.com>
2017-11-30 01:51:03 +0100

    Implemented rest of interpreter instructions

---
+++ src/InterpreterConstants.java
@@ -0,0 +1,15 @@
+
+public class InterpreterConstants {
+	
+	public static final int STACK_SIZE = 1000;
+	
+	public static final boolean SHOW_STORE = true;
+
+	public static int getStackSize() {
+		return STACK_SIZE;
+	}
+
+	public static boolean isShowStore() {
+		return SHOW_STORE;
+	}
+}
---
+++ src/LogicCode.java
@@ -0,0 +1,16 @@
+
+public enum LogicCode {
+	AND(1),
+	OR(2),
+	NEGATION(3);
+	
+	private final int code;
+	
+	private LogicCode(int code) {
+		this.code = code;
+	}
+
+	public int getCode() {
+		return code;
+	}
+}
