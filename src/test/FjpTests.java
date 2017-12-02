d7e237828c8113fe8b8e0497ed595dac073b3849
Tomáš Šimandl <tomas.simandl@gmail.com>
2017-12-03 00:12:41 +0100

    Testing #20
    import testing libs

---
+++ src/test/FjpTests.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FjpTests {
+
+    @Test
+    public void testTry() {
+        assertEquals(10, 10);
+    }
+
+}
