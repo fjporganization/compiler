a6bc6f90ddb63865a2e609a7ab7281f7f27e0603
Ondrej Skala <ondra1237@gmail.com>
2017-12-04 01:02:09 +0100

    Testing #20
    + conditions tests

---
+++ src/test/compiler/ConditionsTests.java
@@ -0,0 +1,26 @@
+package test.compiler;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ConditionsTests extends CompilerTestsBase{
+
+	@Test
+	@DisplayName("Simple condition (no else)")
+	void testSimpleIf() {
+		testFiles("conditionsTests/simpleIf", "conditionsTests/simpleIf.pl0");
+	}
+	
+	@Test
+	@DisplayName("If-else condition")
+	void testIfElse() {
+		testFiles("conditionsTests/ifElse", "conditionsTests/ifElse.pl0");
+	}
+	
+	@Test
+	@DisplayName("Ternary operator")
+	void testTernaryOperator() {
+		testFiles("conditionsTests/ternary", "conditionsTests/ternary.pl0");
+	}
+
+}
