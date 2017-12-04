d2241554ee76bed079c94f0a454dac4e115479b8
skalaon <ondra1237@gmail.com>
2017-12-04 21:47:13 +0100

    Class for stack end manipulations
    Provides functionality for loading from the end of the stack / storing at the end of the stack at the given index

---
+++ src/fjp/compilers/StackEndManipulations.java
@@ -0,0 +1,30 @@
+package fjp.compilers;
+
+import fjp.structures.Instruction;
+import fjp.structures.InstructionCodes;
+
+public class StackEndManipulations {
+	
+	/**
+     * Adds instructions to load value from the stack at the given position from the end of the stack
+     * @param position position from end of the stack
+     * @param data compiler data shared across all compiler classes
+     */
+	public static void loadFromStackEnd(int position, CompilerData data) {
+    	Instruction instruction = new Instruction(InstructionCodes.LOAD, 0, data.getStackPointer() - position);
+		data.toShift.add(instruction);
+		data.addInstructionChangeStackPointer(instruction);
+    }
+    
+    /**
+     * Adds instructions to store value into the stack at the given position from the end of the stack
+     * @param position position from end of the stack
+     * @param data compiler data shared across all compiler classes
+     */
+    public static void storeAtStackEnd(int position, CompilerData data) {
+    	Instruction instruction = new Instruction(InstructionCodes.STORE, 0, data.getStackPointer() - position);
+		data.toShift.add(instruction);
+		data.addInstructionChangeStackPointer(instruction);
+    }
+
+}
