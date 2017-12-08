4e7d10a33436e936cc3baef0b78b74cfc153150a
Ondrej Skala <ondra1237@gmail.com>
2017-12-09 00:48:41 +0100

    Testing #20
    + IO negative tests

--- src/test/compiler/IOTests.java
+++ src/test/compiler/IOTests.java
@@ -4,17 +4,44 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class IOTests extends CompilerTestsBase{
+	
+	private static final String path = "IOTests/";
 
 	@Test
 	@DisplayName("Integer IO")
 	void testIntIO() {
-		testFiles("IOTests/intIO", "IOTests/intIO.pl0");
+		testFiles(path + "intIO", path + "intIO.pl0");
 	}
 	
 	@Test
 	@DisplayName("Fraction IO")
 	void testRatioIO() {
-		testFiles("IOTests/ratioIO", "IOTests/ratioIO.pl0");
+		testFiles(path + "ratioIO", path + "ratioIO.pl0");
 	}
-
+	
+	@Test
+	@DisplayName("Int in ratio output - implicit conversion")
+	void testRatioOutputInt() {
+		testFiles(path + "ratioOutputInt", path + "ratioOutputInt.pl0");
+	}
+	
+	// negative tests ===========================
+	
+	@Test
+	@DisplayName("Negative - boolean in ratio output")
+	void testRatioOutputBoolean() {
+		testCompilerError(path + "errRatioOutputBoolean", "Error(3, 2): Incompatible data type for ratio output");
+	}
+	
+	@Test
+	@DisplayName("Negative - boolean in int output")
+	void testIntOutputBoolean() {
+		testCompilerError(path + "errIntOutputBoolean", "Error(3, 2): Incompatible data type for integer output");
+	}
+	
+	@Test
+	@DisplayName("Negative - ratio in int output")
+	void testIntOutputRatio() {
+		testCompilerError(path + "errIntOutputRatio", "Error(3, 2): Incompatible data type for integer output");
+	}
 }
