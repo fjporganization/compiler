4af8127262d27b6633d8a1090eb101592b688643
Ondrej Skala <ondra1237@gmail.com>
2017-12-09 22:18:26 +0100

    Testing #22
    + conversion tests (ITR, RTI)

---
+++ src/test/interpreter/ConversionTests.java
@@ -0,0 +1,22 @@
+package test.interpreter;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ConversionTests  extends InterpreterTestsBase{
+	
+	private static final String path = "/conversions/";
+	
+	@Test
+	@DisplayName("RTI - float to integer")
+	void testRti(){
+		testOutput(path + "rti.pl0", path + "rti.txt");
+	}
+	
+	@Test
+	@DisplayName("ITR - integer to float")
+	void testItr(){
+		testOutput(path + "itr.pl0", path + "itr.txt");
+	}
+
+}
