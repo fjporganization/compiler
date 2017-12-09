934ac97f484bd944672c5b94204e3a4f83a8b8b9
Ondrej Skala <ondra1237@gmail.com>
2017-12-09 22:27:29 +0100

    Testing interpreter closes #22
    + heap allocation / release tests (NEW, DEL)

---
+++ src/test/interpreter/HeapTests.java
@@ -0,0 +1,21 @@
+package test.interpreter;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class HeapTests extends InterpreterTestsBase{
+	
+	private static final String path = "/heap/";
+	
+	@Test
+	@DisplayName("Allocate on heap")
+	void testNew(){
+		testOutput(path + "new.pl0", path + "new.txt");
+	}
+	
+	@Test
+	@DisplayName("Release on heap")
+	void testDel(){
+		testOutput(path + "del.pl0", path + "del.txt"); //we test that there is no error during execution
+	}
+}
