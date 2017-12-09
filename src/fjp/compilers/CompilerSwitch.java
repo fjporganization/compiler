83107c5b71a3b5f1ee7ca0d5ec38ce56b147e854
Tomáš Šimandl <tomas.simandl@gmail.com>
2017-12-09 10:23:17 +0100

    code clean + code doc #24

--- src/fjp/compilers/CompilerSwitch.java
+++ src/fjp/compilers/CompilerSwitch.java
@@ -16,23 +16,35 @@ import fjp.structures.*;
  */
 public class CompilerSwitch extends CBaseListener {
 
-    /** Data which are shared across all compilers classes */
+    /**
+     * Data which are shared across all compilers classes
+     */
     private final CompilerData data;
 
-    /** Stack of nested switches */
+    /**
+     * Stack of nested switches
+     */
     private Stack<SwitchObject> switches;
 
     /**
-     * Class contains all needed data of one switch
+     * Class contains all data which are need for compile of one switch.
      */
     private class SwitchObject {
+        /** Store instructions which need change operand. */
         Queue<Instruction> insQueue;
+        /** Store instructions which need to set operand to end of switch. */
         List<Instruction> insList;
-        Instruction loadCmpValue [] = null;
+        /** Contains instructions to load values to compare(input expression). */
+        Instruction loadCmpValue[] = null;
+        /** Number of values on stack which are in input expression. */
         int valueLenght;
+        /** Data type of value in input expression. */
         DataType valueType;
 
-        SwitchObject(){
+        /**
+         * Initialize new switch object.
+         */
+        SwitchObject() {
             insQueue = new LinkedBlockingQueue<>();
             insList = new ArrayList<>();
             this.loadCmpValue = null;
@@ -42,9 +54,10 @@ public class CompilerSwitch extends CBaseListener {
 
     /**
      * Constructor of CompilerSwitch
+     *
      * @param data contains data which are shared with all compiler classes
      */
-    public CompilerSwitch(CompilerData data){
+    public CompilerSwitch(CompilerData data) {
         this.data = data;
         switches = new Stack<>();
     }
@@ -58,7 +71,7 @@ public class CompilerSwitch extends CBaseListener {
     }
 
     /**
-     * Set all left jump instructions to jump at the end of switch
+     * Set all left jump instructions to jump at the end of switch and remove values left values from stack.
      */
     @Override
     public void exitSwitchcondition(CParser.SwitchconditionContext ctx) {
@@ -66,16 +79,15 @@ public class CompilerSwitch extends CBaseListener {
 
         int endAdr = data.getCurrentInstructionAddress() + 1;
 
-        for(Instruction ins : obj.insList){
+        for (Instruction ins : obj.insList) {
             ins.setOperand(endAdr);
         }
 
-        for (Instruction ins : obj.insQueue){
+        for (Instruction ins : obj.insQueue) {
             ins.setOperand(endAdr);
         }
 
-        // TODO expecting that value from expression is at the end of switch on top of stack
-        // TODO exist instruction for remove value from stack?
+        // expecting that value from expression is at the end of switch on top of stack
         // remove last value from stack
         for (int i = 0; i < obj.valueLenght; i++) {
             data.addInstruction(new Instruction(InstructionCodes.CONDITIONAL_JUMP, 0, data.getCurrentInstructionAddress() + 2));
@@ -83,9 +95,9 @@ public class CompilerSwitch extends CBaseListener {
         }
     }
 
-
     /**
-     * Set address of jump in last case to start of this case.
+     * If it is first case block set initialize values in switch object.
+     * Set address of jump in last case to start in this case.
      */
     @Override
     public void enterSwitchcase(CParser.SwitchcaseContext ctx) {
@@ -93,7 +105,7 @@ public class CompilerSwitch extends CBaseListener {
         // new instruction will be added in all switch case branches
         SwitchObject mSwitch = switches.peek();
 
-        if(mSwitch.loadCmpValue == null) {
+        if (mSwitch.loadCmpValue == null) {
             mSwitch.valueType = data.popDataType();
             mSwitch.valueLenght = mSwitch.valueType == DataType.FRACTION ? 2 : 1;
             mSwitch.loadCmpValue = new Instruction[mSwitch.valueLenght];
@@ -106,13 +118,13 @@ public class CompilerSwitch extends CBaseListener {
 
         // Get jump from last case block to set address
         Instruction lastJump = mSwitch.insQueue.poll();
-        if(lastJump != null){
+        if (lastJump != null) {
             lastJump.setOperand(data.getCurrentInstructionAddress() + 1);
         }
     }
 
     /**
-     * Set address to jump from last case and add comparison of case
+     * Set address to jump from last case and add comparison of case.
      */
     @Override
     public void enterSwitchstatement(CParser.SwitchstatementContext ctx) {
@@ -120,7 +132,7 @@ public class CompilerSwitch extends CBaseListener {
         Instruction lastJump = switches.peek().insQueue.poll();
 
         DataType type = data.popDataType();
-        if(type != switches.peek().valueType){
+        if (type != switches.peek().valueType) {
             String msg = "Incompatible data type. Expected: " + switches.peek().valueType + " but is: " + type;
             Error.throwError(ctx, msg);
             return;
@@ -131,19 +143,20 @@ public class CompilerSwitch extends CBaseListener {
             data.addInstruction(switches.peek().loadCmpValue[i]);
             data.incStackPointer();
         }
-        
-        if(type == DataType.FRACTION) { // must convert fractions to common divisor when data type is ratio
-        	//multiply numerator of first fraction by denominator of second fraction
-        	StackEndManipulations.loadFromStackEnd(3, data);
-        	data.addInstruction(new Instruction(InstructionCodes.OPERATION, 0, OperationCode.MULTIPLICATION));
-        	data.decStackPointer();
-        	StackEndManipulations.storeAtStackEnd(3, data); //save to index of first fraction numerator
-        	
-        	//multiply denominator of first fraction by numerator of second fraction
-        	data.addInstruction(new Instruction(InstructionCodes.OPERATION, 0, OperationCode.MULTIPLICATION));
-        	data.decStackPointer();
+
+        // must convert fractions to common divisor when data type is ratio
+        if (type == DataType.FRACTION) {
+            //multiply numerator of first fraction by denominator of second fraction
+            StackEndManipulations.loadFromStackEnd(3, data);
+            data.addInstruction(new Instruction(InstructionCodes.OPERATION, 0, OperationCode.MULTIPLICATION));
+            data.decStackPointer();
+            StackEndManipulations.storeAtStackEnd(3, data); //save to index of first fraction numerator
+
+            //multiply denominator of first fraction by numerator of second fraction
+            data.addInstruction(new Instruction(InstructionCodes.OPERATION, 0, OperationCode.MULTIPLICATION));
+            data.decStackPointer();
         }
-        
+
         data.addInstruction(new Instruction(InstructionCodes.OPERATION, 0, OperationCode.EQUALITY));
         data.decStackPointer();
 
@@ -153,17 +166,17 @@ public class CompilerSwitch extends CBaseListener {
         switches.peek().insQueue.offer(jump);
         data.decStackPointer();
 
-        if(lastJump != null){
+        if (lastJump != null) {
             lastJump.setOperand(data.getCurrentInstructionAddress() + 1);
         }
     }
 
     /**
-     * Check key word break at the end of case. Add jump to body of next case.
+     * Check key word 'break' at the end of case. Add jump to body of next case.
      */
     @Override
     public void exitSwitchstatement(CParser.SwitchstatementContext ctx) {
-        if(ctx.BREAKKEYWORD() != null) {
+        if (ctx.BREAKKEYWORD() != null) {
             Instruction breakJump = new Instruction(InstructionCodes.JUMP, 0, -1);
             data.addInstruction(breakJump);
             switches.peek().insList.add(breakJump);
@@ -180,7 +193,7 @@ public class CompilerSwitch extends CBaseListener {
     @Override
     public void enterSwitchdefaultstatement(CParser.SwitchdefaultstatementContext ctx) {
 
-        for(int i = 0; i < 2; i ++) {
+        for (int i = 0; i < 2; i++) {
             // Get jump from last case block to set address
             Instruction lastJump = switches.peek().insQueue.poll();
 
